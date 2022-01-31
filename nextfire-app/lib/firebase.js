f5e535bd01eb81a2d56b21b814e149b87d66efa4
Estefan Hu <ehu@goodwaygroup.com>
2022-01-30 18:46:20 -0800

    added field protection

--- nextfire-app/lib/firebase.js
+++ nextfire-app/lib/firebase.js
@@ -45,3 +45,4 @@ export function postToJSON(doc) {
 }
 
 export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
